5d871634acbe73a31f37bf680fb6e45439d5660e
Jasmine Mai <Jasmine.W.Mai.20@dartmouth.edu>
2021-01-05 14:01:27 -0800

    debugging aws panic

--- main.go
+++ main.go
@@ -169,12 +169,15 @@ func main() {
 					Host:   v.GetString("fqdn") + v.GetString("primary.address"),
 				}
 
+				logging.Info(logger).Log(logging.MessageKey(), "ABOUT TO INITIALIZE")
+
 				factory.Initialize(primaryRouter.Router, selfURL, v.GetString("soa.provider"), webhookHandler, logger, awsMetrics, time.Now)
 			},
 			func(lc fx.Lifecycle, webhookFactory *webhook.Factory, svc xwebhook.Service, logger log.Logger) {
-				webhookFactory.SetExternalUpdate(createArgusSynchronizer(svc, logger))
 				lc.Append(fx.Hook{
 					OnStart: func(context.Context) error {
+						logging.Info(logger).Log(logging.MessageKey(), "INSIDE START")
+						webhookFactory.SetExternalUpdate(createArgusSynchronizer(svc, logger))
 						// wait for DNS to propagate before subscribing to SNS
 						if err = webhookFactory.DnsReady(); err == nil {
 							logging.Info(logger).Log(logging.MessageKey(), "server is ready to take on subscription confirmations")
